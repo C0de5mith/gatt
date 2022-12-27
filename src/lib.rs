2f47ce380548562a2d5dd356cdb29b43a74dcd9b
C0de5mith <aaronseanbailey@gmail.com>
2022-12-27 09:32:05 -0600

    Add parse_uuid macro

--- src/lib.rs
+++ src/lib.rs
@@ -129,7 +129,7 @@
 //! dual licensed as above, without any additional terms or conditions.!
 pub use crate::registration::{CharacteristicProperties, Registration};
 pub use crate::server::Server;
-pub use att::Uuid;
+pub use att::{Uuid, parse_uuid};
 
 mod attribute;
 pub mod characteristics;
